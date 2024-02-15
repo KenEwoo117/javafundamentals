df07161ac9311f0f3dc850d62aca689bd52ee7e1
Kenny <kennethwoods123@gmail.com>
2024-02-15 16:17:50 -0600

    Lesson 3 - My application overview

---
+++ Javafundementals/src/main/java/com/fundamentals/main/Boat.java
@@ -0,0 +1,11 @@
+package com.fundamentals.main;
+
+public class Boat{
+    public String hullColor;
+    public String hullType;
+    public String boatSize;
+    public int totalSeats;
+
+
+    public void boatFunction()  { System.out.println("This boat floats"); }
+}
--- Javafundementals/src/main/java/com/fundamentals/main/Main.java
+++ Javafundementals/src/main/java/com/fundamentals/main/Main.java
@@ -4,6 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
+        houseExample();
+        boatExample();
+    }
+
+    // Single comment
+    /*Multi-line comment
+    This is a second line
+    and a third.
+    * */
+
+    public static void houseExample() {
+        House myHouse = new House();
+        myHouse.doorFunction();
+        myHouse.entranceDoorColor = "Red";
+        myHouse.foundationType = "Concrete";
+        myHouse.roofStyle = "Cedar Shingles";
+        myHouse.totalWindows = 30;
+        System.out.println(myHouse.entranceDoorColor);
+    }
+
+
+    public static void boatExample(){
+        Boat myBoat = new Boat();
+        myBoat.boatFunction();
+        myBoat.hullColor = "Silver";
+        myBoat.hullType = "Steel";
+        myBoat.boatSize = "Large";
+        myBoat.totalSeats = 5;
+        System.out.println(myBoat.totalSeats);
+
     }
 
 }
