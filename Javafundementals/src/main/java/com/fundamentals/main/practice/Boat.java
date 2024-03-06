d4172ecbcf060303e461495bf5a93b210ac501d1
Kenny <kennethwoods123@gmail.com>
2024-03-06 02:49:26 -0600

    Class Practice

--- Javafundementals/src/main/java/com/fundamentals/main/practice/Boat.java
+++ Javafundementals/src/main/java/com/fundamentals/main/practice/Boat.java
@@ -1,2 +1,63 @@
-package com.fundamentals.main.practice;public class Boat {
+package com.fundamentals.main.practice;
+
+public class Boat {
+private String hullColor;
+private String hullType;
+private String boatSize;
+private int totalSeats;
+
+    public Boat(String hullColor){
+        this(hullColor,"aluminum","small",6);
+    }
+
+
+    public Boat(String hullColor, String hullType, String boatSize
+        ,int totalSeats){
+        this.hullColor = hullColor;
+        this.hullType = hullType;
+        this.boatSize = boatSize;
+        this.totalSeats = totalSeats;
+    }
+
+    public void setHullColor(String hullColor) {
+        this.hullColor = hullColor;
+    }
+
+    public String getHullColor() {
+        return hullColor;
+    }
+
+    public void setHullType(String hullType) {
+        this.hullType = hullType;
+    }
+
+    public String getHullType() {
+        return hullType;
+    }
+
+    public void setBoatSize(String boatSize) {
+        this.boatSize = boatSize;
+    }
+
+    public String getBoatSize() {
+        return boatSize;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public static void main(String[] args){
+        Boat myBoat = new Boat("Black", "Aluminum","Small",8);
+        System.out.println(myBoat.getHullColor());
+        System.out.println(myBoat.totalSeats);
+
+    }
+
+
+
 }
--- Javafundementals/src/main/java/com/fundamentals/main/practice/CompanyInfo.java
+++ Javafundementals/src/main/java/com/fundamentals/main/practice/CompanyInfo.java
@@ -1,2 +1,27 @@
-package com.fundamentals.main.practice;public class CompanyInfo {
+package com.fundamentals.main.practice;
+
+/* Practice for using Static */
+public class CompanyInfo {
+
+    static String company = "Your Company";
+
+    int id;
+    String name;
+
+    public void display() {
+        System.out.println(" id: " + id + " Name:"+
+                name + " Company: " + company);
+    }
+
+    public static void main(String[] args){
+        CompanyInfo info = new CompanyInfo();
+        info.id = 10010;
+        info.name = "Peter Parker";
+        info.display();
+        // update static variable
+        CompanyInfo.company = "Daily Bugle";
+        // call display a second time.
+        info.display();
+    }
+
 }
