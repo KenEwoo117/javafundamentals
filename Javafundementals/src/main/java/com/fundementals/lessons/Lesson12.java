f652e436418a1aa5e14be69f07680c9d738e2301
Kenny <kennethwoods123@gmail.com>
2024-03-06 02:49:02 -0600

    Lessons 9 - 12

--- Javafundementals/src/main/java/com/fundementals/lessons/Lesson12.java
+++ Javafundementals/src/main/java/com/fundementals/lessons/Lesson12.java
@@ -1,2 +1,55 @@
-package com.fundementals.lessons;public class Lesson12 {
-}
+package com.fundementals.lessons;
+
+import java.sql.SQLOutput;
+
+/* Lesson 12  Constructors and Encapsulation */
+public class Lesson12 {
+
+
+    private String firstName, lastName;
+
+    // default constructor
+    public Lesson12() {
+        System.out.println("Default Constructor called");
+    }
+
+    public Lesson12(String firstName) {
+        //System.out.println("First Name: " + firstName);
+        this.firstName = firstName;
+    }
+
+    public Lesson12(String firstName, String lastName){
+        //System.out.println("First Name: "+firstName);
+        //System.out.println("Last Name: "+ lastName);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void sendMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        //Lesson12 myLesson = new Lesson12();
+        Lesson12 myLesson = new Lesson12("Kenny", "Woods");
+        myLesson.sendMessage("Welcome to Lesson 12");
+        System.out.println(myLesson.getFirstName());
+
+    }
+}// End class
